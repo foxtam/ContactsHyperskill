eeeec3fe62c3964900ceb7cda07715ca6ab9cd42
Foxtam <foxtam7@ya.ru>
2021-02-26 13:00:28 +0300

    class SimplePhoneBook implements PhoneBook

--- Contacts/task/src/contacts/phonebook/SimplePhoneBook.java
+++ Contacts/task/src/contacts/phonebook/SimplePhoneBook.java
@@ -5,21 +5,26 @@ import contacts.contact.Record;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimplePhoneBook {
+public class SimplePhoneBook implements PhoneBook {
     private final List<Record> list = new ArrayList<>();
 
+
+    @Override
     public void add(Record contact) {
         list.add(contact);
     }
 
+    @Override
     public boolean isEmpty() {
         return size() == 0;
     }
 
+    @Override
     public int size() {
         return list.size();
     }
 
+    @Override
     public void removeAt(int index) {
         list.remove(index);
     }
@@ -33,6 +38,7 @@ public class SimplePhoneBook {
         return builder.toString();
     }
 
+    @Override
     public Record get(int index) {
         return list.get(index);
     }
