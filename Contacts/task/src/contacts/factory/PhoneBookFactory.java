fca8f5e625289a0384fc2b3fca9f73c6542db8fe
Foxtam <foxtam7@ya.ru>
2021-02-26 15:24:46 +0300

    Add interface PhoneBookFactory

--- Contacts/task/src/contacts/factory/PhoneBookFactory.java
+++ Contacts/task/src/contacts/factory/PhoneBookFactory.java
@@ -1,24 +1,7 @@
 package contacts.factory;
 
-import contacts.phonebook.*;
+import contacts.phonebook.PhoneBook;
 
-import java.io.File;
-
-public class PhoneBookFactory {
-    private File file;
-
-    public PhoneBookFactory() {
-    }
-
-    public PhoneBookFactory(File file) {
-        this.file = file;
-    }
-
-    public PhoneBook createPhoneBook() {
-        if (file == null) {
-            return new SimplePhoneBook();
-        } else {
-            return new FilePhoneBook(file);
-        }
-    }
+public interface PhoneBookFactory {
+    PhoneBook createPhoneBook();
 }
