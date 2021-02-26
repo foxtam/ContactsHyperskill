6dfe4549ec8a3b17f55d540644485c81f2be75c7
Foxtam <foxtam7@ya.ru>
2021-02-26 12:28:50 +0300

    Rename PhoneBook -> SimplePhoneBook

--- Contacts/task/src/contacts/phonebook/SimplePhoneBook.java
+++ Contacts/task/src/contacts/phonebook/SimplePhoneBook.java
@@ -5,7 +5,7 @@ import contacts.contact.Record;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PhoneBook {
+public class SimplePhoneBook {
     private final List<Record> list = new ArrayList<>();
 
     public void add(Record contact) {
