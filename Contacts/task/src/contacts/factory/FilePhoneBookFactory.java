9c5d79fda26342b585e4cbd4f24e902d36931593
Foxtam <foxtam7@ya.ru>
2021-02-26 15:24:19 +0300

    Add FilePhoneBookFactory

---
+++ Contacts/task/src/contacts/factory/FilePhoneBookFactory.java
@@ -0,0 +1,30 @@
+package contacts.factory;
+
+import contacts.exceptions.CreateFileException;
+import contacts.phonebook.FilePhoneBook;
+import contacts.phonebook.PhoneBook;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FilePhoneBookFactory implements PhoneBookFactory {
+    private final File file;
+
+    public FilePhoneBookFactory(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public PhoneBook createPhoneBook() {
+        if (!file.isFile()) {
+            try {
+                if (!file.createNewFile()) {
+                    throw new CreateFileException();
+                }
+            } catch (IOException e) {
+                throw new CreateFileException();
+            }
+        }
+        return new FilePhoneBook(file);
+    }
+}
