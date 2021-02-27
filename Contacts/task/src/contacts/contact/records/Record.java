1f254fc648cc67b4ebe905456b81cd4b38d62d52
Foxtam <foxtam7@ya.ru>
2021-02-27 17:48:37 +0300

    Move Records

--- Contacts/task/src/contacts/contact/records/Record.java
+++ Contacts/task/src/contacts/contact/records/Record.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 public interface Record {
     Collection<String> propertiesAsStrings();
 
+    String retrieveProperty(String field);
+
     void updateProperty(String field, String value);
 
     String fullInfo();
